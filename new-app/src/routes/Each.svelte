d05d1af625733210d7f49530deedcc6ecb504c59
ShwetaMishra0101 <shweta.aith@gmail.com>
2024-09-12 13:10:34 +0530

    kyed each blocks

--- new-app/src/routes/Each.svelte
+++ new-app/src/routes/Each.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
     let cats = [
         {id:1, name:"masai"},
         {id:2, name:"svelte"}
@@ -20,4 +20,45 @@
     li{
         color: red;
     }
-</style>
+</style> -->/
+
+
+
+<script>
+    import Things from "./Things.svelte";
+
+    let things = [
+        {id:1, color: 'darlblue'},
+        {id:2, color:'red'},
+        {id:3, color: 'gold'},
+        {id:4,  color:'teal'},
+        {id:5, color: 'black'},
+        {id:6, color: 'deeppink'}
+    ]
+
+    const handleClick = () =>{
+        things = things.slice(1);
+
+    }
+</script>
+
+<button on:click={handleClick}> remove first things</button>
+
+<div style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 1em">
+    <div>
+        <h2>keyed</h2>
+        {#each things as thing (thing.id)}
+        <Things current={thing.color}/>
+        {/each}
+    </div>
+
+    <div>
+        <h2>unkeyed</h2>
+        {#each things as thing}
+            <Things current={thing.color}/>
+        {/each}
+    </div>
+</div>
+
+
+
